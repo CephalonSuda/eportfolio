68040c48d6f6ed9f60eae122220728a809e71d82
CephalonSuda <yigitsahin.work@gmail.com>
2023-10-14 21:31:11 +0200

    finished landing screen

--- js/script.js
+++ js/script.js
@@ -2,10 +2,6 @@
 
 function init() {
     console.log("window loaded");
-    const container = document.querySelector("body");
-    const h1El = document.createElement("h1");
-    h1El.innerText = "Wazup";
-    container.append(h1El);
 }
 
 
