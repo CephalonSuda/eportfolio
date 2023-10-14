702b4f54d43aa01688f1c97c79f827a0916a2d10
CephalonSuda <yigitsahin.work@gmail.com>
2023-10-15 01:37:00 +0200

    Skills section finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,56 @@
+import skills from "../jsons/skills.json" assert { type: "json" };
+
+const largeScreenSize = 990;
+const mediumScreenSize = 672;
+
 "use strict";
 
-function init() {
-    console.log("window loaded");
+function changeSkillRowAmount(x) {
+    const skillsContainer = document.querySelector("#skills ul.skills-container");
+    const screenWidth = x?.currentTarget?.innerWidth ?? window.screen.width;
+    
+    if (screenWidth <= mediumScreenSize) {
+        skillsContainer.style["grid-template-columns"] = `repeat(${Math.ceil(skills.length/3)}, 100px)`;
+    } else if (screenWidth <= largeScreenSize) {
+        skillsContainer.style["grid-template-columns"] = `repeat(${Math.ceil(skills.length/3)}, 150px)`;
+    } else {
+        skillsContainer.style["grid-template-columns"] = `repeat(${Math.ceil(skills.length/4)}, 150px)`;
+    }
 }
 
+function fillSkills() {
 
+    const skillsContainer = document.querySelector("#skills ul.skills-container");
+    
+
+    for (const skill of skills){
+
+        const img = document.createElement("img");
+        img.src = `images/logos/${skill.image}`;
+        img.alt = skill.alt;
+
+        const a = document.createElement("a");
+        a.href = skill.link;
+        a.target = "_blank";
+
+        const li = document.createElement("li");
+        li.classList.add("skill-item");
+
+        a.append(img);
+        li.append(a);
+
+        skillsContainer.append(li)
+    }
+
+}
+
+function init() {
+
+    changeSkillRowAmount();
+    window.addEventListener("resize", changeSkillRowAmount)
+    
+    fillSkills();
+
+}
 
 window.onload = init;
