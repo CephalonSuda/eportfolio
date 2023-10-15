0ac84b44f9117a3288d06701a1ee3b5a4ab4b733
CephalonSuda <yigitsahin.work@gmail.com>
2023-10-15 02:47:29 +0200

    play visible animation once

--- js/script.js
+++ js/script.js
@@ -48,8 +48,6 @@ function observeHiddenElements() {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
-            } else {
-                entry.target.classList.remove("visible");
             }
         })
     });
